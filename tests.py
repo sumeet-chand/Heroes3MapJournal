a95d3f2c482c41e2ee5b5e98252878dbb28ebba2
Sumeet Singh <sumeet.singhji@outlook.com>
2024-06-03 03:36:34 +1000

    v0.1

--- tests.py
+++ tests.py
@@ -7,7 +7,7 @@ class TestGUI(unittest.TestCase):
         """
         CI/CD workflow triggers on pushing this repo to upstream on Github.
         Github actions will build binaries for Windows, MacOS, and Linux
-        using command: pyinstaller --onefile --noconsole --icon=assets/view_earth.ico --distpath=dist Heroes3MapLiker.py
+        using command: pyinstaller --onefile --noconsole --icon=assets/view_earth.ico --distpath=. Heroes3MapLiker.py
         (explanation: create one binary which wont start terminal on binary start e.g. like -mwindows, and embed the icon file
         and place binary in dist folder online so that below tests.py relative filepaths can find them.
         This test then runs to determine if each binary for each OS closes with exit code 0
@@ -16,9 +16,9 @@ class TestGUI(unittest.TestCase):
             Return code. 0 = test passed. 1 = test failed.
         """
         # Adjusted file paths for GitHub Actions workflow
-        windows_binary_path = 'dist/Heroes3MapLiker.exe'
-        macos_binary_path = 'dist/Heroes3MapLiker'
-        linux_binary_path = 'dist/Heroes3MapLiker'
+        windows_binary_path = 'Heroes3MapLiker.exe'
+        macos_binary_path = 'Heroes3MapLiker'
+        linux_binary_path = 'Heroes3MapLiker'
 
         # Run binary and capture output
         windows_completed_process = subprocess.run([windows_binary_path], capture_output=True, text=True)
