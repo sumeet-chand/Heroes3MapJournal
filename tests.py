6ed25f87152ba2e445c04e5680df95d163af9a14
Sumeet Singh <sumeet.singhji@outlook.com>
2024-06-09 04:04:41 +1000

    v0.1

--- tests.py
+++ tests.py
@@ -32,11 +32,11 @@ class TestGUI(unittest.TestCase):
             Return code. 0 = test passed. 1 = test failed.
         """
         if sys.platform == 'win32' and not running_headless:  # Check if running on Windows
-            binary_path = 'Heroes3MapLiker.exe'
+            binary_path = 'build/Heroes3MapLiker.exe'
         elif sys.platform == 'darwin' and not running_headless:  # Check if running on macOS
-            binary_path = 'Heroes3MapLiker_macos'
+            binary_path = 'build/Heroes3MapLiker_macos'
         elif sys.platform.startswith('linux') and not running_headless:  # Check if running on Linux and not headless
-            binary_path = 'Heroes3MapLiker_linux'
+            binary_path = 'build/Heroes3MapLiker_linux'
         else:
             self.skipTest("Skipping test cannot find Operating System")
             return
